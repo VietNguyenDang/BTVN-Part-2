cc0d87a39d4aeb842d4dd703c4c9fe57af60dee6
VietNguyen2004 <85068521+VietNguyen2004@users.noreply.github.com>
2021-06-13 15:46:42 +0700

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float a,b;
+    cin>>a>>b;
+    cout<<fixed<<setprecision(2);
+    cout<<b*a/2<<" ";
+    cout<<b*b/2;
+}
