8db8ad549b486c183e7b8dd68eedb9621e052218
VietNguyen2004 <85068521+VietNguyen2004@users.noreply.github.com>
2021-06-13 14:20:56 +0700

    Create 30.cpp

---
+++ 30.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int max1,max2;
+    cin>>max1;
+    max2 = max1;
+    for(int i =1;i<n;i++)
+    {
+        int k;
+        cin>>k;
+        if(k > max1)
+        {
+            max2 = max1;
+            max1 = k;
+        }
+    }
+    cout<<max2;
+}
